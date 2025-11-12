3466826496f0b131e709068030462b7125ecf78a
ojide chibundo <chibundojuliet21@gmail.com>
2025-11-12 19:30:59 +0100

    Add JS fundamentals scripts

---
+++ datatype.js
@@ -0,0 +1,14 @@
+let name = "ojide Chibundo Juliet";
+let age = 25
+let isStudent = true;
+let address = {city: "Agulu", Country: "Nigeria" };
+let hobbies = ["coding", "watching tech videos", "reading"];
+let undefinedVariable;
+let nullVariable = null;
+console.log("Name:", name, "| Type:", typeof name);
+console.log("Age:", age, "| Type:", typeof age);
+console.log("isStudent:", isStudent, "| Type:", typesof isStudent);
+console.log("address:",  address,  "| Type:", typesof address);
+console.log("Hobbies:", hobbies, "| Type:", typesof hobbies);
+console.log("undefined variable:", undefinedVariable, "| Type:", typeof undefineVarible);   
+console.log("Null Varible:", nullVariable, "| Type:", typeof nullVariable);
