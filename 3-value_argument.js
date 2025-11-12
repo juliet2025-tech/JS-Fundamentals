7b89c36c025ca55b38d963e85fa0228848d96aba
ojide chibundo <chibundojuliet21@gmail.com>
2025-11-12 23:54:34 +0100

    Add 3-value_argument.js script

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+// Step 3: Access the first argument
+const firstArg = process.argv[2];
+
+// Step 4: Check if the argument exists
+if (firstArg) {
+    console.log(firstArg);
+} else {
+    console.log("No argument");
+}
