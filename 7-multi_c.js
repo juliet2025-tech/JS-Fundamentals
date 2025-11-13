75ffbfabb54c02f2eb77eee978af055833ea958c
ojide chibundo <chibundojuliet21@gmail.com>
2025-11-13 12:29:46 +0100

    Fix output for negative and zero cases

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
-const x = process.argv[2];
-const num = parseInt(x);
+const arg = process.argv[2];
+const num = parseInt(arg);
 
-if (!num || num <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else if (num > 0) {
   for (let i = 0; i < num; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
