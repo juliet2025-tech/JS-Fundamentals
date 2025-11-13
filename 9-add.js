cc1daaa8177fa99d3800a4b4c963a96541aaee11
ojide chibundo <chibundojuliet21@gmail.com>
2025-11-13 19:16:05 +0100

    Add 9-add.js for addition assignment

---
+++ 9-add.js
@@ -0,0 +1,6 @@
+const num1 = parseInt(process.argv[2]); // first number
+const num2 = parseInt(process.argv[3]); // second number
+function add(a, b) {
+    return a + b;
+}
+console.log(add(num1, num2));
